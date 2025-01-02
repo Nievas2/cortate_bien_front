284da86c76c483eafeaa6503cf572fd8fe299008
Nievas2 <angelgabrielnievas@gmail.com>
2025-01-02 13:08:12 -0300

    feat(Navbar): :art: Compositization items

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -11,12 +11,11 @@ const Navbar = () => {
   const { logOut } = useLogout()
   const { authUser } = useAuthContext()
   const [open, setOpen] = useState(false)
-  const [cartOpen, setCartOpen] = useState(false)
 
   const { pathname } = useLocation()
   useEffect(() => {
     // Deshabilitar scroll al abrir el modal
-    if (cartOpen || open) {
+    if (open) {
       document.body.style.overflow = "hidden"
     } else {
       document.body.style.overflow = "auto"
@@ -26,14 +25,14 @@ const Navbar = () => {
     return () => {
       document.body.style.overflow = "auto"
     }
-  }, [cartOpen, open])
+  }, [open])
   return (
     <nav
       className={`sticky w-full top-0 z-50 transition-colors duration-200 ${
         pathname == "/" ? "bg-gray-main" : "bg-white"
       }`}
     >
-      <div className="mx-auto max-w-7xl p-4 2xl:pl-0">
+      <div className="mx-auto max-w-8xl p-4 2xl:pl-0">
         <div className="relative flex h-10 items-center gap-10">
           <div className="flex items-center justify-between w-full">
             {/* mobile buttons */}
@@ -73,39 +72,22 @@ const Navbar = () => {
               {/* Navlinks */}
               <div className="hidden md:ml-6 md:flex md:flex-1">
                 <div className="flex w-full items-center justify-end text-center flex-row gap-2 ">
-                  <NavLink
-                    onClick={GoToTop}
-                    to="/"
-                    className={`rounded-md px-3 py-2 text-sm font-medium relative group hover:text-yellow-main transition-all duration-300 ${
-                      pathname === "/" ? "text-yellow-main" : "text-black-main"
-                    }`}
-                  >
-                    Inicio
-                    <span
-                      className={`h-[2.5px] inline-block bg-yellow-main absolute left-1/2 -translate-x-1/2 bottom-[1px] transition-[width] ease duration-[400ms] ${
-                        pathname === "/" ? "w-[80%]" : "w-0"
-                      }`}
-                    >
-                      &nbsp;
-                    </span>
-                  </NavLink>
+                  <NavbarItemDestock
+                    location="/"
+                    pathname={pathname}
+                    name="Inicio"
+                  />
 
-                  {/* Cart button desktop */}
+                  {/* User session */}
                   {authUser != null ? (
-                    <div>
-                      <button
-                        type="button"
-                        className="px-3 py-2 inline-flex items-center justify-center rounded-md text-black-main hover:bg-gray-main/80 hover:text-black-main focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                        aria-controls="mobile-menu"
-                        aria-expanded={cartOpen}
-                        onClick={() => setCartOpen(!cartOpen)}
-                      >
-                        <span className="sr-only">Open main menu</span>
-
-                        {/* Open */}
-                        <Icon icon="mdi:cart" width="24" height="24" />
-                      </button>
-                    </div>
+                    <Button
+                      variant="auth"
+                      onClick={logOut}
+                      className="relative group"
+                    >
+                      Cerrar sesion
+                      <div className="absolute -inset-1 bg-gradient-to-r from-blue-main to-blue-main rounded-lg blur-md opacity-0 group-hover:opacity-60 transition duration-200 group-hover:duration-200" />
+                    </Button>
                   ) : (
                     <>
                       <Link
@@ -113,14 +95,17 @@ const Navbar = () => {
                         onClick={GoToTop}
                         className="relative group"
                       >
-                        <Button variant="auth" className="">
-                          Iniciar sesion
-                        </Button>
+                        <Button variant="auth">Iniciar sesion</Button>
                         <div className="absolute -inset-1 bg-gradient-to-r from-blue-main to-blue-main rounded-lg blur-md opacity-0 group-hover:opacity-60 transition duration-200 group-hover:duration-200" />
                       </Link>
 
-                      <Link to="/auth/registrarse" onClick={GoToTop}>
+                      <Link
+                        to="/auth/registrarse"
+                        onClick={GoToTop}
+                        className="relative group"
+                      >
                         <Button variant="auth">Registrarse</Button>
+                        <div className="absolute -inset-1 bg-gradient-to-r from-blue-main to-blue-main rounded-lg blur-md opacity-0 group-hover:opacity-60 transition duration-200 group-hover:duration-200" />
                       </Link>
                     </>
                   )}
@@ -160,59 +145,22 @@ const Navbar = () => {
               </button>
             </div>
 
-            <div className="flex flex-col gap-4">
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  `block rounded-md px-3 py-2 text-base font-medium ${
-                    isActive
-                      ? "bg-blue-main/50 text-white"
-                      : "text-white hover:bg-gray-main/80 hover:text-white"
-                  } transition-colors duration-300`
-                }
-                onClick={() => {
-                  GoToTop()
-                  setOpen(false)
-                }}
-              >
-                Inicio
-              </NavLink>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2">
+                <NavbarItemMobile
+                  name="Inicio"
+                  location="/"
+                  pathname={pathname}
+                  setOpen={setOpen}
+                />
 
-              <NavLink
-                to="/test"
-                className={({ isActive }) =>
-                  `block rounded-md px-3 py-2 text-base font-medium ${
-                    isActive
-                      ? "bg-blue-main/50 text-white"
-                      : "text-white hover:bg-gray-main/80 hover:text-white"
-                  } transition-colors duration-300`
-                }
-                onClick={() => {
-                  GoToTop()
-                  setOpen(false)
-                }}
-              >
-                test
-              </NavLink>
-
-              {authUser?.user.role == "admin" && (
-                <NavLink
-                  to="admin"
-                  className={({ isActive }) =>
-                    `block rounded-md px-3 py-2 text-base font-medium ${
-                      isActive
-                        ? "bg-gray-main text-black-main"
-                        : "text-black-main hover:bg-gray-main/80 hover:text-black-main"
-                    } transition-colors duration-300`
-                  }
-                  onClick={() => {
-                    GoToTop()
-                    setOpen(false)
-                  }}
-                >
-                  Administracion
-                </NavLink>
-              )}
+                <NavbarItemMobile
+                  name="test"
+                  location="/test"
+                  pathname={pathname}
+                  setOpen={setOpen}
+                />
+              </div>
 
               {authUser ? (
                 <Button variant="auth" onClick={logOut}>
@@ -250,3 +198,61 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+function NavbarItemDestock({
+  location,
+  pathname,
+  name,
+}: {
+  location: string
+  pathname: string
+  name: string
+}) {
+  return (
+    <NavLink
+      onClick={GoToTop}
+      to={location}
+      className={`rounded-md px-3 py-2 text-sm font-medium relative group hover:text-yellow-main transition-all duration-300 ${
+        pathname === location ? "text-yellow-main" : "text-black-main"
+      }`}
+    >
+      {name}
+      <span
+        className={`h-[2.5px] inline-block bg-yellow-main absolute left-1/2 -translate-x-1/2 bottom-[1px] transition-[width] ease duration-[400ms] ${
+          pathname === location ? "w-[80%]" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </NavLink>
+  )
+}
+
+function NavbarItemMobile({
+  location,
+  pathname,
+  name,
+  setOpen,
+}: {
+  location: string
+  pathname: string
+  name: string
+  setOpen: Function
+}) {
+  return (
+    <NavLink
+      to={location}
+      className={`block rounded-md px-3 py-2 text-base font-medium ${
+        location === pathname
+          ? "bg-blue-main/50 text-white"
+          : "text-white hover:bg-gray-main/80 hover:text-white border border-gray-main"
+      } transition-colors duration-300`}
+      onClick={() => {
+        GoToTop()
+        setOpen(false)
+      }}
+    >
+      {name}
+    </NavLink>
+  )
+}
