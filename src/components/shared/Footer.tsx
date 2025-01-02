b47dc74ef428021a7041a3e272850bd3149e1e6c
Nievas2 <angelgabrielnievas@gmail.com>
2025-01-02 13:07:20 -0300

    feat(Footer): :sparkles: Add footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,26 @@
+import LinkedinIcon from "@/utils/LinkedinIcon"
+import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className=" flex items-center justify-center w-full h-20 gap-8 bg-gray-main ">
+      <Link
+        to="https://www.linkedin.com/in/gabriel-nievas/"
+        className="flex flex-row gap-1 justify-center items-center hover:scale-110 transition-transform"
+        target="_blank"
+      >
+        <LinkedinIcon />
+        Gabi
+      </Link>
+      <Link
+        to="https://www.linkedin.com/in/emiigonzalez33/"
+        className="flex flex-row gap-1 justify-center items-center hover:scale-110 transition-transform"
+        target="_blank"
+      >
+        <LinkedinIcon />
+        Emi
+      </Link>
+    </footer>
   )
 }
 export default Footer
