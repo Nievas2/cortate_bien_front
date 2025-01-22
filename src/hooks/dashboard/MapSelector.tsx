19eb17d9d0be90ac28bf590dbcbdbdb467a33ecb
Nievas2 <angelgabrielnievas@gmail.com>
2025-01-22 10:24:20 -0300

    style(Dashboard): :lipstick: Add skin to map

--- src/hooks/dashboard/MapSelector.tsx
+++ src/hooks/dashboard/MapSelector.tsx
@@ -1,43 +1,43 @@
-import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import IconMarker from "@/components/IconMarker";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import IconMarker from "@/components/IconMarker"
 
 const MapSelector = ({
   position,
 }: {
-  position: { lat: number; lng: number };
+  position: { lat: number; lng: number }
 }) => {
-  console.log(position);
+  console.log(position)
 
   return (
     <div>
       <MapContainer
         center={{ lat: position.lat, lng: position.lng }}
         zoom={13}
-        className="w-full h-52"
+        className="w-full h-64"
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+          url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png"
+          attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Pointer position={position} />
         <UpdateView position={position} />
       </MapContainer>
     </div>
-  );
-};
+  )
+}
 
-export default MapSelector;
+export default MapSelector
 
 function Pointer({ position }: { position: { lat: number; lng: number } }) {
-  return <Marker position={position} icon={IconMarker}></Marker>;
+  return <Marker position={position} icon={IconMarker}></Marker>
 }
 
 function UpdateView({ position }: { position: { lat: number; lng: number } }) {
-  const map = useMap();
+  const map = useMap()
 
   // Actualiza la vista cuando la posición cambie
-  map.setView([position.lat, position.lng], map.getZoom());
+  map.setView([position.lat, position.lng], map.getZoom())
 
-  return null;
+  return null
 }
