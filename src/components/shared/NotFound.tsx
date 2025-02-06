a722a1ba9a06526ea0f564dbc1232e69694eacbb
Nievas2 <angelgabrielnievas@gmail.com>
2025-02-06 11:31:20 -0300

    style(NotFound): :lipstick: Add notfound page

--- src/components/shared/NotFound.tsx
+++ src/components/shared/NotFound.tsx
@@ -1,6 +1,28 @@
-const NotFound = () => {
+import GradientText from "@/utils/functions/GradientText";
+
+export default function NotFound({ children }: { children: React.ReactNode }) {
   return (
-    <div>NotFound</div>
+    <div className="flex items-center justify-center w-full">
+      <div className="flex flex-col gap-4 max-w-md w-full bg-gray-main shadow-lg rounded-lg p-8 text-center">
+        <GradientText
+          colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+          animationSpeed={3}
+          showBorder={false}
+          className="text-4xl leading-12 font-bold text-white"
+        >
+          Algo salio mal.
+        </GradientText>
+
+        <p className="text-xl text-white">
+          No hemos encontrado lo que buscabas.
+        </p>
+
+        <p className="text-lg text-white">
+          No te preocupes, podemos ayudarte a encontrar lo que necesitas.
+        </p>
+
+        {children}
+      </div>
+    </div>
   )
 }
-export default NotFound
--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,6 +1,20 @@
+import NotFound from "@/components/shared/NotFound"
+import { Button } from "@/components/ui/button"
+import { Icon } from "@iconify/react/dist/iconify.js"
+import { Link } from "react-router-dom"
+
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <main className="flex items-center justify-center w-full min-h-screen">
+      <NotFound>
+        <Link className="flex items-center justify-center w-full" to="/">
+          <Button variant="simple" className="flex gap-2">
+            <Icon icon="carbon:home" />
+            Ir al inicio
+          </Button>
+        </Link>
+      </NotFound>
+    </main>
   )
 }
 export default NotFoundPage
