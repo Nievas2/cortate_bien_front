c39e71061b088def4397ebe3e2c45e719f35c39e
Nievas2 <angelgabrielnievas@gmail.com>
2025-03-20 20:36:22 -0300

    fix(Dashboard): :bug: Add token to post image

--- src/services/ImagesService.ts
+++ src/services/ImagesService.ts
@@ -1,8 +1,15 @@
 import axios from "axios"
+import Cookies from "js-cookie"
 
+const token = Cookies.get("token")
 export async function postImage(image: any) {
   try {
-    const res = axios.post(import.meta.env.VITE_API_URL_IMAGES, image)
+    const res = axios.post(import.meta.env.VITE_API_URL_IMAGES, image, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    })
     return res
   } catch (error) {
     throw error
