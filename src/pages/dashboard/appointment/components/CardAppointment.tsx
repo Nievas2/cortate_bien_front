98de6965052f0f60a643379ed051838e584a9cdb
Nievas2 <angelgabrielnievas@gmail.com>
2025-04-04 11:30:26 -0300

    fix(CardAppointment): :sparkles: Refactor appointment handling logic for improved state management

--- src/pages/dashboard/appointment/components/CardAppointment.tsx
+++ src/pages/dashboard/appointment/components/CardAppointment.tsx
@@ -227,143 +227,148 @@ const CardAppointment = ({
         </section>
       </div>
 
-      <div className="flex gap-4 w-full justify-end">
-        {appointment.estado !== "CONFIRMADO" &&
-          appointment.estado !== "REPROGRAMADO" && (
+      {appointment.estado == "COMPLETADO" ? (
+        ""
+      ) : (
+        <div className="flex gap-4 w-full justify-end">
+          {appointment.estado !== "CONFIRMADO" &&
+            appointment.estado !== "REPROGRAMADO" && (
+              <Button
+                variant="secondary"
+                onClick={() => {
+                  setOpen(true)
+                  setState("CONFIRMADO")
+                }}
+              >
+                Aceptar
+              </Button>
+            )}
+
+          {appointment.estado !== "REPROGRAMADO" && (
+            <Button
+              variant="destructive"
+              onClick={() => {
+                setOpen(true)
+                setState("CANCELADO")
+              }}
+            >
+              Cancelar
+            </Button>
+          )}
+
+          {appointment.estado == "CONFIRMADO" && (
             <Button
               variant="secondary"
               onClick={() => {
                 setOpen(true)
-                setState("CONFIRMADO")
+                setState("COMPLETADO")
               }}
             >
-              Aceptar
+              Completar
             </Button>
           )}
 
-        {appointment.estado !== "REPROGRAMADO" && (
-          <Button
-            variant="destructive"
-            onClick={() => {
-              setOpen(true)
-              setState("CANCELADO")
-            }}
-          >
-            Cancelar
-          </Button>
-        )}
+          <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent forceMount>
+              <DialogHeader>
+                <DialogTitle>
+                  {state == "CONFIRMADO"
+                    ? "Confirmación de turno"
+                    : state == "COMPLETADO"
+                      ? "Finalizar el turno"
+                      : "Cancelación de turno"}
+                </DialogTitle>
+              </DialogHeader>
+              {successStatus ? (
+                <div className="flex flex-col gap-4">
+                  <span>Turno actualizado</span>
+                  <Button
+                    variant="simple"
+                    onClick={() => {
+                      setOpen(false)
+                      setSuccessStatus(false)
+                    }}
+                  >
+                    Cerrar
+                  </Button>
+                </div>
+              ) : (
+                <div className="flex flex-col gap-8">
+                  {appointment.estado === "CANCELADO" ? (
+                    <span>
+                      El turno se encuentra cancelado y no puede ser cambiado.
+                    </span>
+                  ) : (
+                    <>
+                      <div className="flex flex-col gap-4 items-center justify-center">
+                        <div className="flex flex-col gap-2 font-extralight items-center">
+                          <p>
+                            {state === "CONFIRMADO"
+                              ? "¿Estás seguro de confirmar el turno?"
+                              : state === "COMPLETADO"
+                                ? "¿Estás seguro de completar el turno?"
+                                : "¿Estás seguro de cancelar el turno?"}
+                          </p>
+                          <p>
+                            Esta accion no se puede deshacer, por favor confirma
+                            que quieres{" "}
+                            {state === "CONFIRMADO"
+                              ? "confirmar"
+                              : state === "COMPLETADO"
+                                ? "completar"
+                                : "cancelar"}{" "}
+                            el turno.
+                          </p>
+                        </div>
+                        <div className="flex justify-between gap-2 w-full">
+                          <Button
+                            variant="secondary"
+                            onClick={() => {
+                              if (state === "CONFIRMADO") {
+                                setValueState("state", "CONFIRMADO")
+                              }
+                              if (state === "COMPLETADO") {
+                                setValueState("state", "COMPLETADO")
+                              }
+                              if (state === "CANCELADO") {
+                                setValueState("state", "CANCELADO")
+                              }
+                              mutateUpdate({ state })
+                            }}
+                            type="button"
+                          >
+                            Confirmar
+                          </Button>
 
-        {appointment.estado == "CONFIRMADO" && (
-          <Button
-            variant="secondary"
-            onClick={() => {
-              setOpen(true)
-              setState("COMPLETADO")
-            }}
-          >
-            Completar
-          </Button>
-        )}
-
-        <Dialog open={open} onOpenChange={setOpen}>
-          <DialogContent forceMount>
-            <DialogHeader>
-              <DialogTitle>
-                {state == "CONFIRMADO"
-                  ? "Confirmación de turno"
-                  : state == "COMPLETADO"
-                    ? "Finalizar el turno"
-                    : "Cancelación de turno"}
-              </DialogTitle>
-            </DialogHeader>
-            {successStatus ? (
-              <div className="flex flex-col gap-4">
-                <span>Turno actualizado</span>
-                <Button
-                  variant="simple"
-                  onClick={() => {
-                    setOpen(false)
-                  }}
-                >
-                  Cerrar
-                </Button>
-              </div>
-            ) : (
-              <div className="flex flex-col gap-8">
-                {appointment.estado === "CANCELADO" ? (
-                  <span>
-                    El turno se encuentra cancelado y no puede ser cambiado.
-                  </span>
-                ) : (
-                  <>
-                    <div className="flex flex-col gap-4 items-center justify-center">
-                      <div className="flex flex-col gap-2 font-extralight items-center">
-                        <p>
-                          {state === "CONFIRMADO"
-                            ? "¿Estás seguro de confirmar el turno?"
-                            : state === "COMPLETADO"
-                              ? "¿Estás seguro de completar el turno?"
-                              : "¿Estás seguro de cancelar el turno?"}
-                        </p>
-                        <p>
-                          Esta accion no se puede deshacer, por favor confirma
-                          que quieres{" "}
-                          {state === "CONFIRMADO"
-                            ? "confirmar"
-                            : state === "COMPLETADO"
-                              ? "completar"
-                              : "cancelar"}{" "}
-                          el turno.
-                        </p>
+                          <Button
+                            variant="ghost"
+                            type="button"
+                            onClick={() => setOpen(false)}
+                          >
+                            Cancelar
+                          </Button>
+                        </div>
                       </div>
-                      <div className="flex justify-between gap-2 w-full">
-                        <Button
-                          variant="secondary"
-                          onClick={() => {
-                            if (state === "CONFIRMADO") {
-                              setValueState("state", "CONFIRMADO")
-                            }
-                            if (state === "COMPLETADO") {
-                              setValueState("state", "COMPLETADO")
-                            }
-                            if (state === "CANCELADO") {
-                              setValueState("state", "CANCELADO")
-                            }
-                            mutateUpdate({ state })
-                          }}
-                          type="button"
-                        >
-                          Confirmar
-                        </Button>
 
-                        <Button
-                          variant="ghost"
-                          type="button"
-                          onClick={() => setOpen(false)}
-                        >
-                          Cancelar
-                        </Button>
-                      </div>
-                    </div>
-
-                    {errorsState.root?.message && (
-                      <span className="text-red-500 text-sm">
-                        {errorsState.root.message}
-                      </span>
-                    )}
-                    {errorUpdate instanceof AxiosError &&
-                      errorUpdate.response && (
+                      {errorsState.root?.message && (
                         <span className="text-red-500 text-sm">
-                          {errorUpdate.response.data.message}
+                          {errorsState.root.message}
                         </span>
                       )}
-                  </>
-                )}
-              </div>
-            )}
-          </DialogContent>
-        </Dialog>
-      </div>
+                      {errorUpdate instanceof AxiosError &&
+                        errorUpdate.response && (
+                          <span className="text-red-500 text-sm">
+                            {errorUpdate.response.data.message}
+                          </span>
+                        )}
+                    </>
+                  )}
+                </div>
+              )}
+            </DialogContent>
+          </Dialog>
+        </div>
+      )}
     </main>
   )
 }
