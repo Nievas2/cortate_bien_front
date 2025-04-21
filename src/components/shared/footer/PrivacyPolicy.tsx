db6b4e37466b8b88f8c1ce5d09396bc830b94d16
Nievas2 <angelgabrielnievas@gmail.com>
2025-04-21 14:43:02 -0300

    fix(PrivacyPolicy, TermsAndConditions): :bug: Update titles and content to reflect accurate privacy policy information

--- src/components/shared/footer/PrivacyPolicy.tsx
+++ src/components/shared/footer/PrivacyPolicy.tsx
@@ -3,7 +3,7 @@ const PrivacyPolicy = () => {
     <div className="flex flex-col gap-8 items-center justify-center p-6">
       <div className="flex flex-col gap-2 items-center justify-center">
         <h1 className="text-3xl font-bold tracking-tighter leading-16 lg:leading-20 text-white sm:text-4xl md:text-5xl lg:text-6xl/none max-w-[80vw]">
-          Términos y Condiciones
+          Política de Privacidad
         </h1>
 
         <span className="text-sm font-light">
@@ -19,42 +19,46 @@ const PrivacyPolicy = () => {
 
       <ol className="flex flex-col gap-3 ">
         <li className="first-letter:font-bold">
-          1. Uso del servicio Cortate Bien está diseñado para gestionar turnos.
-          No debes usar nuestra aplicación para actividades ilegales o no
-          autorizadas.
+          1. Información que recopilamos Información personal: Nombre, correo
+          electrónico, número de teléfono, etc. Información técnica: Dirección
+          IP, tipo de navegador, sistema operativo, cookies, etc. Datos de uso:
+          Cómo interactúas con nuestra aplicación, páginas visitadas, duración
+          de las sesiones, etc.
         </li>
 
         <li className="first-letter:font-bold">
-          2. Cuentas de usuario Es tu responsabilidad mantener la seguridad de
-          tu cuenta. Proporciona información precisa y actualizada al
-          registrarte.
+          2. Cómo usamos la información Para personalizar tu experiencia en
+          Cortate Bien. Para mejorar nuestros servicios y funcionalidades. Para
+          enviarte notificaciones importantes o información promocional (si lo
+          aceptas).
         </li>
 
         <li className="first-letter:font-bold">
-          3. Propiedad intelectual Todos los derechos de autor, marcas
-          registradas y contenido pertenecen a Cortate Bien. No puedes copiar,
-          modificar ni distribuir nuestro contenido sin permiso.
+          3. Cómo protegemos tu información Usamos medidas de seguridad
+          estándar, como cifrado y firewalls, para proteger tus datos.
         </li>
 
         <li className="first-letter:font-bold">
-          4. Limitaciones de responsabilidad Cortate Bien no será responsable de
-          daños indirectos, pérdida de datos o cualquier inconveniente derivado
-          del uso de la aplicación.
+          4. Compartir información con terceros No compartiremos tu información
+          con terceros, excepto en los siguientes casos: Cumplimiento de
+          requisitos legales. Proveedores de servicios necesarios para operar la
+          aplicación.
         </li>
 
         <li className="first-letter:font-bold">
-          5. Terminación del servicio Nos reservamos el derecho de suspender o
-          eliminar tu cuenta si violas estos términos.
+          5. Tus derechos Puedes acceder, rectificar o eliminar tus datos
+          personales. Para ejercer estos derechos, contáctanos en
+          angelgabrielnievas@gmail.com.
         </li>
 
         <li className="first-letter:font-bold">
-          6. Cambios en los términos Podemos modificar estos términos en
-          cualquier momento. Te notificaremos sobre cambios significativos. Al
-          usar Cortate Bien, aceptas cumplir con estos términos. Si tienes
-          preguntas, contáctanos en angelgabrielnievas@gmail.com.
+          6. Cambios en esta política Nos reservamos el derecho de modificar
+          esta política en cualquier momento. Te notificaremos de cambios
+          significativos. Si tienes preguntas, no dudes en escribirnos a
+          angelgabrielnievas@gmail.com.
         </li>
       </ol>
     </div>
-  )
-}
-export default PrivacyPolicy
+  );
+};
+export default PrivacyPolicy;
