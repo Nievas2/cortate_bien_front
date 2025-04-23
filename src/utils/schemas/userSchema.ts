85a2cb9af6e9b8c2d5d5d8d8d437171288f2907b
Nievas2 <angelgabrielnievas@gmail.com>
2025-04-23 16:07:48 -0300

    fix(userSchema): :bug: Remove password and confirmPassword fields from updateUserSchema

--- src/utils/schemas/userSchema.ts
+++ src/utils/schemas/userSchema.ts
@@ -18,38 +18,6 @@ export const updateUserSchema = z
       )
       .default(new Date()),
     tipoDeCuenta: z.enum(["BARBERO", "CLIENTE"]).default("CLIENTE"),
-    password: z
-      .string()
-      .nonempty("La contraseña es requerida")
-      .min(8, "La contraseña debe tener al menos 8 caracteres")
-      .max(30, "La contraseña no puede tener más de 30 caracteres")
-      .refine((value) => /^(?=.*[a-z])/.test(value), {
-        message: "La contraseña debe tener al menos una letra minúscula",
-      })
-      .refine((value) => /^(?=.*[A-Z])/.test(value), {
-        message: "La contraseña debe tener al menos una letra mayúscula",
-      })
-      .refine((value) => /^(?=.*[0-9])/.test(value), {
-        message: "La contraseña debe tener al menos un número",
-      }),
-    confirmPassword: z
-      .string()
-      .nonempty("La contraseña es requerida")
-      .min(8, "La contraseña debe tener al menos 8 caracteres")
-      .max(30, "La contraseña no puede tener más de 30 caracteres")
-      .refine((value) => /^(?=.*[a-z])/.test(value), {
-        message: "La contraseña debe tener al menos una letra minúscula",
-      })
-      .refine((value) => /^(?=.*[A-Z])/.test(value), {
-        message: "La contraseña debe tener al menos una letra mayúscula",
-      })
-      .refine((value) => /^(?=.*[0-9])/.test(value), {
-        message: "La contraseña debe tener al menos un número",
-      }),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Las contraseñas no coinciden",
-    path: ["confirmPassword"],
   })
 
 export const recoveryPasswordSchema = z
