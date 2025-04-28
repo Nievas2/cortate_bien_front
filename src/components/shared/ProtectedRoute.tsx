f5b8bd91b8fe5212460e9170416ddedc1677e577
Nievas2 <angelgabrielnievas@gmail.com>
2025-04-28 16:33:09 -0300

    feat(General): :sparkles: Add validation user with role is PENDING_REGISTER

--- src/components/shared/ProtectedRoute.tsx
+++ src/components/shared/ProtectedRoute.tsx
@@ -1,13 +1,22 @@
-import { useAuthContext } from '@/contexts/authContext';
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useAuthContext } from "@/contexts/authContext"
+import React from "react"
+import { Navigate, useLocation } from "react-router-dom"
 
-export const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { authUser } = useAuthContext();
+export const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { authUser } = useAuthContext()
+  const { pathname, search } = useLocation()
 
-  if (!authUser) {
-    return <Navigate to="/auth/iniciar-sesion" replace />;
+  if (authUser == null) {
+    return <Navigate to="/auth/iniciar-sesion" replace />
+  }
+  if (
+    authUser.user.rol === "PENDING_REGISTER" &&
+    (pathname !== "/profile" || search !== "?required=true")
+  ) {
+    return <Navigate to="/profile?required=true" replace />
   }
 
-  return <>{children}</>;
-};
+  return <>{children}</>
+}
