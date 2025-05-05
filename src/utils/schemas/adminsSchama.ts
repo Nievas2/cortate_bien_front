36b953af7ecb1da76d61a3e1d844f334c303283c
Nievas2 <angelgabrielnievas@gmail.com>
2025-05-05 10:46:54 -0300

    feat(Admin): Add schema to send notifications

--- src/utils/schemas/adminsSchama.ts
+++ src/utils/schemas/adminsSchama.ts
@@ -3,3 +3,8 @@ import z from "zod"
 export const deleteUserSchema = z.object({
   id: z.string().nonempty(),
 })
+
+export const createNotificactionsSchema = z.object({
+  title: z.string().nonempty("El campo es requerido"),
+  body: z.string().nonempty("El campo es requerido"),
+})
