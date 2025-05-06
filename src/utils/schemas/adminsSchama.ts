6e22299f3153ea0b0dbe94a10234dac3741de6fc
Nievas2 <angelgabrielnievas@gmail.com>
2025-05-06 10:16:51 -0300

    feat(Firebase): Add scheme for token removal

--- src/utils/schemas/adminsSchama.ts
+++ src/utils/schemas/adminsSchama.ts
@@ -8,3 +8,7 @@ export const createNotificactionsSchema = z.object({
   title: z.string().nonempty("El campo es requerido"),
   body: z.string().nonempty("El campo es requerido"),
 })
+
+export const createTokenSchema = z.object({
+  token: z.string().nonempty("El campo es requerido"),
+})
