eabaa3cc2a5cc1ba78fd60d5f5985c23c89f5958
Nievas2 <angelgabrielnievas@gmail.com>
2025-05-08 11:26:21 -0300

    feat(Logout): integrate Firebase token deletion on logout

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,14 +1,26 @@
 import { useAuthContext } from "@/contexts/authContext"
-import Cookies from "js-cookie"
+import { deleteTokenFirebase } from "@/services/FirebaseService"
+import { useMutation } from "@tanstack/react-query"
 import { useState } from "react"
+import Cookies from "js-cookie"
 
 export const useLogout = () => {
   const [loading, setLoading] = useState(false)
-  const { setAuthUser } = useAuthContext()
+  const { authUser, setAuthUser } = useAuthContext()
+  const { mutate: deleteToken } = useMutation({
+    mutationKey: ["delete-token-firebase"],
+    mutationFn: async (token: string) => {
+      return deleteTokenFirebase(token)
+    },
+  })
 
   const logOut = () => {
     try {
       setLoading(true)
+      localStorage.removeItem("active")
+      if (authUser?.fcmToken) {
+        deleteToken(authUser?.fcmToken)
+      }
       Cookies.remove("token")
       setAuthUser(null)
     } catch (error) {
